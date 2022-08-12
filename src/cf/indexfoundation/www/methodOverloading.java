31e2c6e7db4e9a208860fb114ebbc06a974344b4
FireIndex <support@indexfoundation.cf>
2022-08-12 18:15:56 +0530

    Method Overloading Upload
    Signed-off-by: FireIndex <support@indexfoundation.cf>

--- src/cf/indexfoundation/www/methodOverloading.java
+++ src/cf/indexfoundation/www/methodOverloading.java
@@ -4,10 +4,13 @@ public class methodOverloading {
     public static void main(String[] args) {
 
         class Subhajit{
-            int findSum(int x, int y){      //The method name is findSum.
+            void findSum(){     //This is the default method.
+                System.out.println("You did not provide any numbers");
+            }
+            int findSum(int x, int y){      //This is the first method. The method name is findSum.
                 return (x * y);
             }
-            int findSum(int x, int y, int z){       //Method name is the same as above, this is called method overloading. you just need to specify more arguments than other method.
+            int findSum(int x, int y, int z){       //This is the second Method. The name is the same as above, this is called method overloading. you just need to specify more arguments than other method.
                 int multiply = (x * y * z);
                 return multiply;
             }
@@ -15,6 +18,7 @@ public class methodOverloading {
 
         Subhajit oneResult = new Subhajit();
 
+        oneResult.findSum();
         System.out.println(oneResult.findSum(5,8));
         System.out.println(oneResult.findSum(6,2,8));
     }
