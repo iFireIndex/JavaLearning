abc37c5ce25017838c51eef2e745a1ce2a16a825
FireIndex <support@indexfoundation.cf>
2022-08-13 23:31:27 +0530

    Tiny Math Using Method Upload
    Signed-off-by: FireIndex <support@indexfoundation.cf>

---
+++ src/cf/indexfoundation/www/methodExample.java
@@ -0,0 +1,25 @@
+package cf.indexfoundation.www;
+
+import java.util.Scanner;
+
+class Discount{
+    double discountAmount;
+    double calculate(double x){
+        discountAmount = x - ((25.00/100.00)*x);
+        return discountAmount;
+    }
+}
+
+public class methodExample {
+    public static void main(String[] args) {
+        Discount discount1 = new Discount();
+        int userGivenAmount;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount of pen");
+        userGivenAmount = sc.nextInt();
+
+        //Displaying the result
+        System.out.println("Amount to Be Paid (Less Discount 25%): " +discount1.calculate(userGivenAmount));
+    }
+}
