cb063cc0659664c1b44dbb54b4280e796ca1fac4
FireIndex <support@indexfoundation.cf>
2022-10-14 13:07:16 +0530

    Upload Perfect Number
    Signed-off-by: FireIndex <support@indexfoundation.cf>

---
+++ src/cf/indexfoundation/www/perfectNumber.java
@@ -0,0 +1,26 @@
+package cf.indexfoundation.www;
+
+import java.util.Scanner;
+
+public class perfectNumber {
+    public static void main(String[] args) {
+        int userInput, sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter A Number To Check If It Is A Perfect OR Not");
+        userInput = scanner.nextInt(); // Input number from user
+
+        for (int i = 1; i < userInput; i++) {   // loop t+ "It is not a Perfect Number : " o find the factors
+            if(userInput % i == 0) {    // Condition for factors of number
+                sum = (sum+ i); // storing sum of factors excluding number
+            }
+        }
+
+        //comparing sum of factors with number
+        if (sum == userInput) {
+            System.out.println(userInput+ " It is a Perfect Number");
+        } else {
+            System.out.println(userInput+ " It is not a Perfect Number");
+        }
+
+    }
+}
